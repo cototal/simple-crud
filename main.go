92e4408f34f9a434f8c9ada8bfbd7c2183d162a4
Shaun <shauntm@gmail.com>
2024-06-01 15:08:48 -0500

    Setup basic CRUD operations.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -60,7 +61,91 @@ func main() {
 	})
 
 	router.HandleFunc("POST /", func(wtr http.ResponseWriter, req *http.Request) {
+		var task Task
+		if err := json.NewDecoder(req.Body).Decode(&task); err != nil {
+			http.Error(wtr, err.Error(), http.StatusBadRequest)
+			return
+		}
 
+		result, err := db.Exec(queries.CreateOneTask(), task.Name)
+		if err != nil {
+			http.Error(wtr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			http.Error(wtr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		task.ID = int(id)
+		wtr.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(wtr).Encode(task)
+	})
+
+	router.HandleFunc("GET /{id}", func(wtr http.ResponseWriter, req *http.Request) {
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil {
+			http.Error(wtr, "Invalid task ID", http.StatusBadRequest)
+			return
+		}
+
+		var task Task
+		err = db.QueryRow(queries.SelectOneTask(), id).Scan(
+			&task.ID, &task.Name)
+
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(wtr, "Task not found", http.StatusNotFound)
+			} else {
+				http.Error(wtr, err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		wtr.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(wtr).Encode(task)
+	})
+
+	router.HandleFunc("PUT /{id}", func(wtr http.ResponseWriter, req *http.Request) {
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil {
+			http.Error(wtr, "Invalid task ID", http.StatusBadRequest)
+			return
+		}
+
+		var task Task
+		if err = json.NewDecoder(req.Body).Decode(&task); err != nil {
+			http.Error(wtr, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		_, err = db.Exec(queries.UpdateOneTask(), task.Name, id)
+
+		if err != nil {
+			http.Error(wtr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		task.ID = id
+		wtr.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(wtr).Encode(task)
+	})
+
+	router.HandleFunc("DELETE /{id}", func(wtr http.ResponseWriter, req *http.Request) {
+		id, err := strconv.Atoi(req.PathValue("id"))
+		if err != nil {
+			http.Error(wtr, "Invalid task ID", http.StatusBadRequest)
+			return
+		}
+
+		_, err = db.Exec(queries.DeleteOneTask(), id)
+		if err != nil {
+			http.Error(wtr, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		wtr.WriteHeader(http.StatusNoContent)
 	})
 
 	server := http.Server{
